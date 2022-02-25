978a7d3a61972df4197734c0a66f2cd6fea0c127
per <per@alipsa.se>
2022-02-25 22:13:17 +0100

    remove unused imports.

--- src/test/java/se/alipsa/rideutils/ReadImageTest.java
+++ src/test/java/se/alipsa/rideutils/ReadImageTest.java
@@ -1,11 +1,9 @@
 package se.alipsa.rideutils;
 
-import javafx.application.Platform;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;;
-import org.testfx.api.FxRobot;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
 
